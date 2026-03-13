cb574581c7d832b7c655f54b436b9823e016ce3f
PIRES014 <everton.reis.pires@escola.pr.gov.br>
2026-03-13 11:07:28 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+n3 = int(input("Digite o terceiro numero: "))
+
+maior = max(n1, n2, n3)
+menor = min(n1, n2, n3)
+
+print(f"maior numero:", maior)
+print(f"menor numero:", menor)
